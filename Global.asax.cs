f35581b340d539d2dab272a0675af420d3e81894
Mo'men <zaaza1233@gmail.com>
2023-03-28 07:12:03 +0200

    70 done

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Vidly.App_Start;
 
 namespace Vidly
 {
@@ -12,6 +13,19 @@ namespace Vidly
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(config =>
+            {
+                var setting = config.Formatters.JsonFormatter.SerializerSettings;
+                setting.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                setting.Formatting = Formatting.Indented;
+                config.MapHttpAttributeRoutes();
+                config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
+            });
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
