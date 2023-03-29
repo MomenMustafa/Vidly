0f3b4865b36c867ea9740298479623934337458c
Mo'men <zaaza1233@gmail.com>
2023-03-29 17:16:37 +0200

    80 done

--- Dtos/CustomerDto.cs
+++ Dtos/CustomerDto.cs
@@ -13,6 +13,8 @@ namespace Vidly.Dtos
 
         public bool IsSubscribeedToNewsLetter { get; set; }
 
+        public MembershipTypeDto MembershipType { get; set; }
+
         public byte MembershipTypeId { get; set; }
 
         //[Min18YearsIfAMember]
