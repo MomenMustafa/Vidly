67c08d02618089c60de68d51ee7c36934a7e619c
Mo'men <zaaza1233@gmail.com>
2023-03-31 22:17:23 +0200

    Added Authentication tables in database

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -1,11 +1,17 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 using Vidly.Models;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationDbContext()
+    : base("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\zaaza\\Desktop\\Vidly\\App_Data\\ApplicationDbContext.mdf;Integrated Security=True", throwIfV1Schema: false)
     {
+    }
 
+    public static ApplicationDbContext Create()
+    {
+        return new ApplicationDbContext();
     }
     public DbSet<Customer> Customers { get; set; }
     public DbSet<MembershipType> MembershipTypes { get; set; }
